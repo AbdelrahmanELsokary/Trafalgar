1ac938c9112f44ef3f7e9e8fd29ab77f24ff318f
Abdelrahman Elsokary <abdalrahmanelsokarey@gmail.com>
2025-01-10 21:36:29 +0200

    Add: Dark Mode Beginner

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+let them = document.getElementById("them");
+them.onclick = () => {
+  if (document.body.classList.contains("dark-mode")) {
+    document.body.classList.remove("dark-mode");
+  } else {
+    document.body.classList.add("dark-mode");
+  }
+};
